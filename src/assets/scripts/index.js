90c8dc97a8c3dad224a50d5bdb46cd5b40792417
tgarciafo <tgarciafo@uoc.edu>
2021-03-22 20:07:11 +0100

    primera part pac1

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    document.querySelector("#menuIcon").addEventListener("click", e => {
+        var x = document.getElementById("menuNav");
+        if (x.className === "menu") {
+            x.className += " responsive";
+        } else {
+            x.className = "menu";
+        }
+    });
+});
