9924f80ad26d98e169a1281f3f0b3e1ef1fec03f
tgarciafo <tgarciafo@uoc.edu>
2021-03-26 18:12:01 +0100

    modificar textos varis del package i index.html i posar codi js a main.js i eliminar index.js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,14 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+ window.addEventListener("load", () => {
+  document.querySelector("#menuIcon").addEventListener("click", e => {
+      var x = document.getElementById("menuNav");
+      if (x.className === "menu") {
+          x.className += " responsive";
+      } else {
+          x.className = "menu";
+      }
+  });
+});
+
